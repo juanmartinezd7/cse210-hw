73c9e963428d43161e4ec03ab58b944955b253d6
rysmartinez <rysmartinez7@gmail.com>
2025-01-08 21:57:30 -0600

    Week 1 Program 3 Stretch

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,14 +5,15 @@ class Program
     static void Main(string[] args)
     {
         
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
 
-        //Console.WriteLine($"You selected {magicNumber} as magic number.");
+        Random generateNumber = new Random();
+        int magicNumber = generateNumber.Next(1, 101);
 
         int guessNumber = -1;
 
-        //Console.WriteLine($"Guess number is {guessNumber}");
+        
         while (guessNumber != magicNumber)
         {
             Console.Write("What is your guess? ");
