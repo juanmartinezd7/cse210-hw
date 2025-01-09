42025ed4cfda36122dc25acaf710df9476285c02
rysmartinez <rysmartinez7@gmail.com>
2025-01-08 21:47:29 -0600

    Finish Week 1 Program 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine($"You selected {magicNumber} as magic number.");
+
+        int guessNumber = -1;
+
+        //Console.WriteLine($"Guess number is {guessNumber}");
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+        
+
+            if (magicNumber > guessNumber)
+            {
+               Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guessNumber)
+            {
+               Console.WriteLine("Lower");
+            }
+            else 
+            {
+               Console.WriteLine("You Guessed it!");
+            }
+
+        }
     }
 }
