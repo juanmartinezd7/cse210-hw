2928ba3a4e70381c6908f4b12b8baeee88e2a6a4
rysmartinez <rysmartinez7@gmail.com>
2025-01-09 14:56:15 -0600

    Finish week1 exercise 4 stretch

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 class Program
 {
@@ -9,10 +7,10 @@ class Program
         List<int> numbers = new List<int>();
         int newNumber = -1;
 
-
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
         while (newNumber != 0)
         {
-            Console.Write("Enter a list of numbers, type 0 when finished. ");
+            Console.Write("Enter a number: ");
             
             string inputNumber = Console.ReadLine();
             newNumber = int.Parse(inputNumber);
@@ -48,6 +46,39 @@ class Program
                   }
                }
                Console.WriteLine($"The largest number is: {largestNumber}");
+
+
+               //get the smallest positive number closest to zero
+               int? closestPositive = null;
+
+               foreach (var number in numbers)
+               {
+                if (number > 0)
+                {
+                    if (closestPositive == null || number < closestPositive)
+                    {
+                        closestPositive = number;
+                    }
+                }
+               }
+                //show the result
+               if (closestPositive.HasValue)
+               {
+                   Console.WriteLine($"The Smallest positive number closest to zero is: {closestPositive}");
+               }
+               else
+               {
+                   Console.WriteLine("There are no positive numbers in the list");
+               }
+                //show the list of numbers sorted
+                Console.WriteLine($"The sorted list is:");
+                numbers.Sort();
+                foreach (var number in numbers)
+                {
+                    
+                    Console.WriteLine(number);
+                }
+
 
 
             }
