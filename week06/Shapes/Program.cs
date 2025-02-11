82f89663121fc0e8a208d259884705b753e211fa
rysmartinez <rysmartinez7@gmail.com>
2025-02-10 18:40:58 -0600

    Week 6 Shapes program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Yellow", 4);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Green", 5, 7);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Red", 3);
+        shapes.Add(shape3);
+;
+
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
